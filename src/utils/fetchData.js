7585337f496de227150806d010acc5e1b84f52d6
EPIWATCH <chenxi.guo@unsw.edu.au>
2023-09-27 16:05:51 +1000

    upload chen fitness

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
 
-
-
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'IoN8git50Kmshpk9GjKmLDxQOnJZp1zZhnRjsnnxAEcwDlh0PA',
+  },
+};
 
 export const fetchData = async (url, options) => {
-    const response = await fetch(url, options);
-    const data = await response.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
 
-    return data;
-}
+  return data;
+};
